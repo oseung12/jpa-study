bafa71f12d00de0f65c16934d0da3bc6ccaf849f
oseung12 <osungg@gmail.com>
2022-07-04 19:20:36 +0900

    jpql

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -19,8 +19,11 @@ public class JpaMain {
           em.persist(member);
 
           TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-          TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-          Query query3 = em.createQuery("select m.username, m.age from Member m"); //반환 타입이 명확하지 않은 경우
+          // TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); // 변환 타입이 명확한 경우
+          // Query query3 = em.createQuery("select m.username, m.age from Member m"); //반환 타입이 명확하지 않은 경우
+
+            Member result = query.getSingleResult();
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e){
