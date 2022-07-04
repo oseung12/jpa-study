39db2726bfe0b99b3957d623791babbc0e2a14d6
oseung12 <osungg@gmail.com>
2022-07-04 19:26:00 +0900

    파라미터바인딩

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -18,12 +18,12 @@ public class JpaMain {
           member.setAge(10);
           em.persist(member);
 
-          TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-          // TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); // 변환 타입이 명확한 경우
-          // Query query3 = em.createQuery("select m.username, m.age from Member m"); //반환 타입이 명확하지 않은 경우
+          //파라미터 바인딩
+          Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                  .setParameter("username", "member1")
+                  .getSingleResult();
 
-            Member result = query.getSingleResult();
-            System.out.println("result = " + result);
+            System.out.println("result = " + result.getUsername());
 
             tx.commit();
         } catch (Exception e){
